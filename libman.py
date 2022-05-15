5eca5f9deb5e1abb3a130eeab221bac1622f0dd5
tanyamistry <tanyamistry21@gmail.com>
2022-05-15 11:28:00 +0530

    second commit

--- libman.py
+++ libman.py
@@ -1,1 +1,18 @@
-print("HELLLLLLO")
+from tkinter import *
+class LibraryManagementSystem:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Library Management System")
+        self.root.geometry("1550x800+0+0")
+
+
+        lbltitle=Label(self.root,text="Library Management System",bg="powder blue",fg="green",bd=20,relief=RIDGE,font=("times new roman",50,"bold"),padx=2,pady=6)
+        lbltitle.pack(side=TOP,fill=X)
+
+
+
+
+if __name__ == "__main__":
+    root= Tk()
+    obj=LibraryManagementSystem(root)
+    root.mainloop()
