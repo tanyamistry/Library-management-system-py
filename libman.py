2ec39c4db585e8f67fe6e33e8bedb913ac36d81b
tanyamistry <tanyamistry21@gmail.com>
2022-05-15 11:39:23 +0530

    commt3

--- libman.py
+++ libman.py
@@ -1,4 +1,7 @@
+from sqlite3 import DatabaseError
 from tkinter import *
+
+from pandas import DataFrame
 class LibraryManagementSystem:
     def __init__(self,root):
         self.root = root
@@ -6,10 +9,14 @@ class LibraryManagementSystem:
         self.root.geometry("1550x800+0+0")
 
 
-        lbltitle=Label(self.root,text="Library Management System",bg="powder blue",fg="green",bd=20,relief=RIDGE,font=("times new roman",50,"bold"),padx=2,pady=6)
+        lbltitle=Label(self.root,text="Library Management System",bg="powder blue",fg="green",bd=15,relief=RIDGE,font=("times new roman",30,"bold"),padx=2,pady=6)
         lbltitle.pack(side=TOP,fill=X)
 
+        frame=Frame(self.root,bd=12,relief=RIDGE,padx=20,bg="powder blue")
+        frame.place(x=0,y=90,width=1530,height=400)
 
+        DataFrameLeft=LabelFrame(frame,text="Library Membership information",bg="powder blue",fg="green",bd=12,relief=RIDGE,font=("times new roman",12,"bold"))
+        DataFrameLeft.place(x=0,y=5,width=900,height=350)
 
 
 if __name__ == "__main__":
